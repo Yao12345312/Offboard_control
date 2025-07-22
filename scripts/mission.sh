1143c0e16c889fdb9216f6dd414707c91d98a55c
huangjunyao123 <148626538+Yao12345312@users.noreply.github.com>
2025-07-22 20:47:51 +0800

    Create mission.sh

---
+++ scripts/mission.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+source /opt/ros/jazzy/setup.bash
+
+source ~/mavros2_ws/install/setup.bash
+#pi5 UART0 connect
+sudo chmod 777 /dev/ttyAMA0
+
+export PATH="$PATH:/home/pi5/.local/bin"
+
+export ROS_DOMAIN_ID=1
+export ROS_MASTER_URI=http://192.168.50.1:11311
+
+ros2 launch tracked2vision tracked2vision.launch.py 
