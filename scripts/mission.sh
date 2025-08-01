143d531194f79b09298a30ff114a2787e05873c1
罗毛 <150697653+maolouis@users.noreply.github.com>
2025-08-02 00:43:21 +0800

    Update mission.sh

--- scripts/mission.sh
+++ scripts/mission.sh
@@ -3,7 +3,9 @@ source /opt/ros/jazzy/setup.bash
 
 source ~/mavros2_ws/install/setup.bash
 #pi5 UART0 connect
+sudo chmod 777 /dev/ttyAMA1
 sudo chmod 777 /dev/ttyAMA0
+sudo chmod 777 /dev/ttyAMA4
 
 export PATH="$PATH:/home/pi5/.local/bin"
 
@@ -11,3 +13,8 @@ export ROS_DOMAIN_ID=1
 export ROS_MASTER_URI=http://192.168.50.1:11311
 
 ros2 launch tracked2vision tracked2vision.launch.py 
+
+ros2 bag record -a
+
+
+exit 0
